6cbc59ac2daf15971e061680b2d5ad16d241c3fe
Andrew <wkilascience@gmail.com>
2021-08-29 02:26:13 +0300

    ewewe

--- script.js
+++ script.js
@@ -1,27 +1,31 @@
 const sections = document.querySelectorAll('.services-ui'),
     tabs = document.querySelectorAll('.tab')
 
-const methods = {
-    classes: {
-        'services-ui' : 'services-active',
-        'tab': 'active-tab'
+// работа
+
+for (const section of sections) {
+    section.addEventListener('click', () => {
+        section.classList.add('services-active')
+        deleteClass(section, 'services-ui')
+    })
+}
+
+function deleteClass(section, name) {
+    let sections = document.querySelectorAll(name)
+    sections.forEach((element) => {
+        element != section ? element.classList.remove(`${searchPos(name)}`) : false
+    })
+}
+
+function searchPos(name) {
+    for (key in classes) {
+        name == key ? classes[key] : false
     }
 }
 
-methods.addClass(sections[0], 'services-active')
+let classes = {
+    'tab': 'active-tab',
+    'services-ui': 'services-active'
+}
 
-
-
-
-
-// работа
-
-// for (const section of sections) {
-//     section.addEventListener('click', methods.addClass(section, 'services-active'))
-// }
-
-// function deleteClass(section) {
-//     sections.forEach((element) => {
-//         element != section ? element.classList.remove('services-active') : false
-//     })
-// }
+searchPos('services-ui')
