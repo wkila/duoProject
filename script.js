5e3719575d273cb5a483fc5f8902adb4e271ea0f
Andrew <wkilascience@gmail.com>
2021-08-29 16:28:36 +0300

    Анимация перемещения
    гыгыгага порвал жепу за анимку

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const sections = document.querySelectorAll('.services-ui'),
-    tabs = document.querySelectorAll('.tab')
+    tabs = document.querySelectorAll('.tab'),
+    draggbles = document.querySelectorAll('.drag')
 
 let methods = {
     classes: {
@@ -34,4 +35,61 @@ for (const tab of tabs) {
         tab.classList.add(`${methods.searchPos('tab')}`)
         methods.deleteClass(tab, 'tab')
     })
+}
+
+// перемещение табов
+
+draggbles.forEach((element) => {
+    element.addEventListener('dragstart', dragStart)
+    element.addEventListener('dragenter', dragEnter)
+    element.addEventListener('dragover', dragOver)
+    element.addEventListener('dragleave', dragLeave)
+    element.addEventListener('drop', dragDrop)
+    element.addEventListener('dragend', dragEnd)
+    
+})
+
+let dragThis = null;
+
+function dragStart(event) {
+    dragThis = this;
+
+    this.classList.add('active-move')
+    setTimeout(() => event.target.classList.add('hide'), 0)
+
+    event.dataTransfer.effectAllowed = 'move';
+    event.dataTransfer.setData('img/html', this.innerHTML);
+}
+
+function dragEnter(event) {
+    this.classList.add('over')
+}
+
+function dragOver(event) {
+    event.preventDefault()
+    this.classList.remove('active-move')
+
+    return false
+}
+
+function dragLeave(event) {
+    this.classList.remove('over')
+}
+
+function dragEnd(event) {
+    draggbles.forEach((el) => {
+        el.classList.remove('hide')
+        el.classList.remove('over')
+        el.classList.remove('active-move')
+    })
+    event.target.className = 'drag'
+}
+
+function dragDrop(event) {
+    if (dragThis != this) {
+        dragThis.innerHTML = this.innerHTML;
+        this.innerHTML = event.dataTransfer.getData('img/html');
+    }
+    
+    return false;
 }
