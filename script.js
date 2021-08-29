71c2b4dbb134f3b16d028737ab5c8f8b0ac7bb5b
Andrew <wkilascience@gmail.com>
2021-08-29 03:17:30 +0300

    update script

--- script.js
+++ script.js
@@ -1,39 +1,37 @@
 const sections = document.querySelectorAll('.services-ui'),
     tabs = document.querySelectorAll('.tab')
 
-// работа
-
-function deleteClass(section, name) {
-    let sections = document.querySelectorAll(`.${name}`)
-    sections.forEach((element) => {
-        element != section ? element.classList.remove(`${searchPos(name)}`) : false
-    })
-}
-
-function searchPos(name) {
-    for (let key in classes) {
-        if (name === key) {
-            return classes[name]
+let methods = {
+    classes: {
+        'tab': 'active-tab',
+        'services-ui': 'services-active'
+    },
+    searchPos(name) {
+        for (let key in this.classes) {
+            if (name === key) {
+                return this.classes[name]
+            }
         }
+    },
+    deleteClass(section, name) {
+        let sections = document.querySelectorAll(`.${name}`)
+        sections.forEach((element) => {
+            element != section ? element.classList.remove(`${this.searchPos(name)}`) : false
+        })
     }
-}
-
-let classes = {
-    'tab': 'active-tab',
-    'services-ui': 'services-active'
 }
 
 
 for (const section of sections) {
     section.addEventListener('click', () => {
         section.classList.add('services-active')
-        deleteClass(section, 'services-ui')
+        methods.deleteClass(section, 'services-ui')
     })
 }
 
 for (const tab of tabs) {
     tab.addEventListener('click', () => {
         tab.classList.add('active-tab')
-        deleteClass(tab, 'tab')
+        methods.deleteClass(tab, 'tab')
     })
 }
